aed6be55734a7dc7baa720c423a16defb8a1c3a1
coltenarave <coltenarave@comcast.net>
2017-09-26 12:53:59 -0600

    Initial commit

---
+++ Chivas_hw3_task2.cpp
@@ -0,0 +1,16 @@
+// File: Chivas_hw3_task2.cpp
+// Created by Colten Arave on 9/26/17 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+
+// Prototypes
+
+// Main Program Program
+int main(void) {
+    return 0;
+}
+// Function Definitions
