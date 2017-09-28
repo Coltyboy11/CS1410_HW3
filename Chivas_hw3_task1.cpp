0a5230b04a720733a573c723400ab1abb0e7ba9f
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2017-09-28 11:21:19 -0600

    Updated main() so when asking for Date2's input it states "Please enter another date"

--- Chivas_hw3_task1.cpp
+++ Chivas_hw3_task1.cpp
@@ -8,7 +8,6 @@ class Date{
     char tempchar;
     public:
     void get_date(){
-        cout << "Please enter a date.(format: 12/25/1995)"<<endl;
         cin>>month>>tempchar>>day>>tempchar>>year;
     }
     void show_date(){
@@ -18,7 +17,9 @@ class Date{
 int main() {
     Date d1;
     Date d2;
+    cout << "Please enter a date.(format: 12/25/1995)"<<endl;
     d1.get_date();
+    cout << "Please enter another date.(format: 12/25/1995)"<<endl;
     d2.get_date();
     cout << "Date 1 = ";
     d1.show_date();
