a2db40453e4bc0216dc9ce4e3657e2e9378f1719
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2017-09-28 08:13:58 -0600

    Added get_date(), and show_date(). Added Date class.

--- Chivas_hw3_task1.cpp
+++ Chivas_hw3_task1.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
-
+class Date{
+    private:
+    int month;
+    int day;
+    int year;
+    char tempchar;
+    public:
+    void get_date(){
+        cout << "Please enter a date.(format: 12/25/1995)";
+        cin>>month>>tempchar>>day>>tempchar>>year;
+    }
+    void show_date(){
+        cout << month<<"/"<<day<<"/"<<year;
+    }
+};
 int main() {
-
+    Date d1;
+    d1.get_date();
+    d1.show_date();
     return 0;
 }
+/*
+int get_date(){
+
+    cout << "Please enter a date (format 12/25/1955): ";
+    cin >>month>>tempchar>>day>>tempchar>>year;
+    return month,day,year;
+
+}
+*/
