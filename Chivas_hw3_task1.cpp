6352d62f59b20aaea68509bf018f9f858efc902a
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2017-09-28 11:03:16 -0600

    Added a second date and a cout to display "Date 1=" and "Date 2="

--- Chivas_hw3_task1.cpp
+++ Chivas_hw3_task1.cpp
@@ -8,17 +8,22 @@ class Date{
     char tempchar;
     public:
     void get_date(){
-        cout << "Please enter a date.(format: 12/25/1995)";
+        cout << "Please enter a date.(format: 12/25/1995)"<<endl;
         cin>>month>>tempchar>>day>>tempchar>>year;
     }
     void show_date(){
-        cout << month<<"/"<<day<<"/"<<year;
+        cout<< month<<"/"<<day<<"/"<<year<<endl;
     }
 };
 int main() {
     Date d1;
+    Date d2;
     d1.get_date();
+    d2.get_date();
+    cout << "Date 1 = ";
     d1.show_date();
+    cout << "Date 2 = ";
+    d2.show_date();
     return 0;
 }
 /*
